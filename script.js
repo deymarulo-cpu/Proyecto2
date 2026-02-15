682e21ef018fd0b7039519eb3f88f572c880dc0f
deymarulo-cpu <deymarulo@gmail.com>
2026-02-14 22:00:25 -0400

    Implement pixel transition in startGame function
    Add pixel creation and transition effects to startGame function.

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function mover() {
   btn.style.top = Math.random() * window.innerHeight + 'px';
   btn.style.left = Math.random() * window.innerWidth + 'px';
 }
+function startGame() {
+  const transition = document.querySelector(".pixel-transition");
+
+  // Crear cuadritos
+  for (let i = 0; i < 400; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    transition.appendChild(pixel);
+
+    setTimeout(() => {
+      pixel.style.transform = "scale(1)";
+    }, Math.random() * 500);
+  }
+
+  // Después ocultar pantalla inicio
+  setTimeout(() => {
+    document.querySelector(".inicio").style.display = "none";
+    transition.innerHTML = "";
+  }, 1200);
+}
