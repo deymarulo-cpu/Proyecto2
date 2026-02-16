21704483b9ca2c149b9834ccf422e95ef336684e
deymarulo-cpu <deymarulo@gmail.com>
2026-02-15 21:52:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+function iniciarTodo() {
+  startGame();
+
+  // Florecer lotos después de desaparecer inicio
+  setTimeout(() => {
+    florecerLotos();
+
+    // Animar texto solo después de mostrar la pantalla principal
+    animarTexto();
+  }, 1200);
+}
+
+function startGame() {
+  const transition = document.querySelector(".pixel-transition");
+
+  for (let i = 0; i < 400; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    transition.appendChild(pixel);
+
+    setTimeout(() => {
+      pixel.style.transform = "scale(1)";
+    }, Math.random() * 500);
+  }
+
+  setTimeout(() => {
+    document.querySelector(".inicio").style.display = "none";
+    document.getElementById("pantallaPrincipal").style.display = "block";
+    transition.innerHTML = "";
+  }, 1200);
+}
+
+// ===== FLORECER LOTOS =====
+function florecerLotos() {
+  const lotos = document.querySelectorAll('.loto');
+
+  lotos.forEach((loto, index) => {
+    setTimeout(() => {
+      loto.classList.add('florecer');
+    }, index * 500);
+  });
+}
+
+// ===== ANIMACIÓN DE TEXTO LÍNEA POR LÍNEA =====
 function animarTexto() {
   const h2 = document.getElementById("bienvenida");
   if (!h2) return;
