2fcdd475f36b2649fad62d6e3a727506cdb98a14
deymarulo-cpu <deymarulo@gmail.com>
2026-02-16 10:19:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -117,23 +117,28 @@ function iniciarTodo() {
       }, 800);
     });
 
-    // ===== PANTALLA FINAL: SI/NO =====
-    document.getElementById("botonSi").addEventListener("click", () => {
-      // REPRODUCIR AUDIO BOTÓN SI
-      song4.currentTime = 0;
-      song4.play();
+   // ===== PANTALLA FINAL: SI/NO =====
+document.getElementById("botonSi").addEventListener("click", () => {
+  // REPRODUCIR AUDIO BOTÓN SI
+  song4.currentTime = 0;
+  song4.play();
 
-    });
+  // OCULTAR pantalla final
+  const pantallaFinal = document.getElementById("pantallaFinal");
+  pantallaFinal.style.display = "none";
 
-    document.getElementById("botonNo").addEventListener("click", () => {
-      const pantallaFinal = document.getElementById("pantallaFinal");
-      pantallaFinal.style.display = "none";
-      const pantallaGameOver = document.getElementById("pantallaGameOver");
-      pantallaGameOver.style.display = "flex";
-    });
+  // MOSTRAR pantalla corazón
+  const pantallaCorazon = document.getElementById("pantallaCorazon");
+  pantallaCorazon.style.display = "flex";
+});
 
-  });
-}
+document.getElementById("botonNo").addEventListener("click", () => {
+  const pantallaFinal = document.getElementById("pantallaFinal");
+  pantallaFinal.style.display = "none";
+
+  const pantallaGameOver = document.getElementById("pantallaGameOver");
+  pantallaGameOver.style.display = "flex";
+});
 
 // ===== START GAME =====
 function startGame(callback) {
