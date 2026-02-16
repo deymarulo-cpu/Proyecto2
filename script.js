c48b57f3b42ded0db7172ca2829384ba5d471f6a
deymarulo-cpu <deymarulo@gmail.com>
2026-02-15 20:45:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,29 +27,7 @@ function startGame() {
     document.querySelector(".inicio").style.display = "none";
     document.getElementById("pantallaPrincipal").style.display = "block";
     transition.innerHTML = "";
-
-    crearEstrellas();
   }, 1200);
-}
-
-// ===== CREAR ESTRELLAS =====
-function crearEstrellas() {
-  const contenedor = document.querySelector(".stars");
-  const colores = ["#ffffff", "#ffd700", "#87cefa", "#ff69b4", "#c084fc"];
-
-  for (let i = 0; i < 150; i++) {
-    const estrella = document.createElement("div");
-    estrella.style.position = "absolute";
-    estrella.style.width = estrella.style.height = Math.random() * 2 + "px";
-    estrella.style.top = Math.random() * 100 + "vh";
-    estrella.style.left = Math.random() * 100 + "vw";
-    estrella.style.background = colores[Math.floor(Math.random() * colores.length)];
-    estrella.style.borderRadius = "50%";
-    estrella.style.opacity = Math.random() * 0.8 + 0.2;
-    estrella.style.animation = `blink ${1 + Math.random() * 2}s infinite alternate`;
-
-    contenedor.appendChild(estrella);
-  }
 }
 
 // ===== FLORECER LOTOS =====
@@ -58,7 +36,7 @@ function florecerLotos() {
 
   lotos.forEach((loto, index) => {
     setTimeout(() => {
-      loto.classList.add('florecer'); // activa animación
-    }, index * 800);
+      loto.classList.add('florecer');
+    }, index * 500); // más juntas en tiempo
   });
 }
